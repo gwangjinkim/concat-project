c34d4aa176982a8afd9c3516f4df25dec781c672
gwangjinkim <gwang.jin.kim.phd@gmail.com>
2025-07-12 18:08:33 +0200

    fix typo

--- src/concat_project/config.py
+++ src/concat_project/config.py
@@ -8,5 +8,5 @@ def parse_args():
     parser.add_argument("--ext", nargs="+", required=True, help="Allowed file extensions (e.g., .lisp, .cl)")
     parser.add_argument("--exclude", nargs="*", default=[], help="Relative filders or files to exclude")
     parser.add_argument("--include-hidden", action="store_true", help="Include hidden files/folders")
-    parser.add_argument("--tree_style", default="unicode", choices=["unicode, "ascii"], help="Style of file tree")
+    parser.add_argument("--tree_style", default="unicode", choices=["unicode", "ascii"], help="Style of file tree")
     return parser.parse_args()
