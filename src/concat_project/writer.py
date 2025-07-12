ebe82a73e7431dd18a0ab6ad2b1d146ae265d70a
gwangjinkim <gwang.jin.kim.phd@gmail.com>
2025-07-12 18:26:30 +0200

    fix typo bug

--- src/concat_project/writer.py
+++ src/concat_project/writer.py
@@ -1,7 +1,7 @@
 def write_output(output_path, base_path, files, tree_string):
     with open(output_path, "w", encoding="utf-8") as out:
         out.write(";;;; FILE STRUCTURE\n")
-        out.write("tree_string" + "\n\n")
+        out.write(f"{tree_string}" + "\n\n")
         
         for rel_path, abs_path in files:
             out.write(f";;;; {rel_path}\n")
